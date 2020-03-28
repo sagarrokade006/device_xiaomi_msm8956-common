beababc8d31c680b84ec09c933c50571fa819b8f
Vishalcj17 <Vishalcj17@users.noreply.github.com>
2020-03-28 00:45:17 +0000

    msm8956-common: remove -qti suffix from power
    *And build missing Power Package
    
    Signed-off-by: Vishalcj17 <vishalcj17@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -364,7 +364,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
+    android.hardware.power@1.2-service \
+    android.hardware.power@1.2-impl \
     android.hardware.power.stats@1.0-service.mock
 
 # Qualcomm dependencies
