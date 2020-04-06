75c5ca2a7093cdb7586456c783683864bbe40081
Sagarrokade006 <sagarrokade006@gmail.com>
2020-04-06 11:07:30 +0000

    Finally Booted

--- msm8956.mk
+++ msm8956.mk
@@ -206,8 +206,6 @@ PRODUCT_PACKAGES += \
     memtrack.msm8952 \
     libdisplayconfig \
     liboverlay \
-    libqdMetaData \
-    libqdMetaData.system \
     libgenlock \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
@@ -332,6 +330,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    libmm-omxcore \
     libc2dcolorconvert \
     libextmedia_jni \
     libOmxAacEnc \
