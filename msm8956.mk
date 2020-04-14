476b3ec30ea836fced2da2a2dcb27c17ca54b28d
Vachounet <Vachounet@users.noreply.github.com>
2020-04-14 15:35:47 +0000

    msm8956-common: import CAF binderized power hal

--- msm8956.mk
+++ msm8956.mk
@@ -360,8 +360,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.2-service \
+    power.qcom
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
