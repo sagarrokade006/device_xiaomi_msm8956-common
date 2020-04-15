bb50eb4783174c138800b977cf4574494ea27d47
Sagarrokade006 <sagarrokade006@gmail.com>
2020-04-15 08:53:09 +0000

    prebuilts: Add CameraGo

--- msm8956.mk
+++ msm8956.mk
@@ -151,9 +151,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
-    Snap \
-    SnapdragonCamera \
-    SnapdragonCamera2 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
@@ -519,3 +516,7 @@ PRODUCT_PACKAGES += \
 # World APN list
 PRODUCT_COPY_FILES += \
     device/xiaomi/msm8956-common/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# CameraGo
+PRODUCT_PACKAGES += \
+    CameraGo
